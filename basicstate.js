09d540270622da6efb2e6d6a8b2d8cd6bcde1432
Gérard Lichtert <gerard.lichtert@hotmail.com>
2024-05-16 15:38:38 +0200

    database now creates tables for chats

--- basicstate.js
+++ basicstate.js
@@ -1,7 +1,14 @@
 module.exports = {
-    setup: (Users, Rooms) => {
-        Rooms.addRoom("random" , {forceMembership: true, description: "Random!"});
-        Rooms.addRoom("general", {forceMembership: true, description: "interesting things"});
-        Rooms.addRoom("private" ,{forceMembership: true, description: "some very private channel", private: true});
-    }
-}
+	setup: (Users, Rooms) => {
+		Rooms.addRoom("random", { forceMembership: true, description: "Random!" });
+		Rooms.addRoom("general", {
+			forceMembership: true,
+			description: "interesting things",
+		});
+		Rooms.addRoom("private", {
+			forceMembership: true,
+			description: "some very private channel",
+			private: true,
+		});
+	},
+};
